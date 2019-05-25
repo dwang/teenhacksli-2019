b7886f3345f4c96ad1034344c73016b42741b9d2
Daniel Wang <hello@danielwang.dev>
2019-05-25 18:13:16 -0400

    feat: update html output

--- server/app.py
+++ server/app.py
@@ -5,9 +5,8 @@ import string
 cnn = newspaper.build("https://lite.cnn.io", memoize_articles=False)
 pattern = re.compile(r"[^a-zA-Z0-9-]")
 index = open("index.html", "w")
-header = '<!doctypehtml><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover"name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
-index.write(header)
-
+header = '<!doctype html><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover" name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
+index.write(header + "<h1>Latest News</h1><br>")
 count = 0
 str1 = basicWeather.getWeather()
 index.write("<center>" + str1 + "</br>" + "</center></br>")
@@ -21,10 +20,9 @@ for article in cnn.articles:
         file_name = pattern.sub('', article_title.replace(" ", "-")).lower() + ".html"
         
         with open("articles/" + file_name, 'w') as out:
-            out.write(header + "<h1>" + article_title + "</h1> <br> <p class='text'>" + article.text + "</p>")
+            out.write(header + "<h1>" + article_title + "</h1> <br> <p class='text'>" + article.text.replace(". ", ". <br>") + "</p>")
 
-
-        index.write("<a href='articles/{}'>".format(file_name) + article_title + "</a><br>")
+        index.write("<a href='articles/{}'>".format(file_name) + article_title + "</a><br><br>")
 
         count += 1
     except Exception as e:
