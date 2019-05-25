60e08b4ec0bee107176a84e97c0fa1f865e69997
Rohit Mukund <rohitmukund38@gmail.com>
2019-05-25 16:37:05 -0500

    Basic functionality completed

--- server/app.py
+++ server/app.py
@@ -1,25 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
-
 import newspaper
+import re
+import string
 
 cnn = newspaper.build("https://lite.cnn.io", memoize_articles=False)
+pattern = re.compile(r"[^a-zA-Z0-9-]")
+index = open("index.html", "w")
+header = '<!doctypehtml><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover"name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
+index.write(header)
+count = 0
 
-@app.route("/")
-def index():
-    article = cnn.articles  
-    count = 0
-    print(len(cnn.articles))
-    for i in article:
-        file = open("article" + str(count), 'w')
-        article[count].download()
-        article[count].parse()
-        article[count].nlp()
-        file.write(cnn.articles[count].text)
-        file.close()
+for article in cnn.articles:
+    try:
+        article.download()
+        article.parse()
+        
+        article_title = article.html.split('<h2 style="margin-top:0px;">')[1].split('</h2>')[0]
+        file_name = pattern.sub('', article_title.replace(" ", "-")).lower() + ".html"
+
+        with open("articles/" + file_name, 'w') as out:
+            out.write(header + "<h1>" + article_title + "</h1> <br> <p class='text'>" + article.text + "</p>")
+
+        index.write("<a href='articles/{}'>".format(file_name) + article_title + "</a><br>")
+
         count += 1
+    except Exception as e:
+        print(e)
 
-    return "200"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+index.close()
