59ee5c52fc7767fb5de5e397bbcfd5aa0bec136c
Daniel Wang <hello@danielwang.dev>
2019-05-25 13:07:15 -0400

    feat: scrape first news article from cnn lite

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+import newspaper
+
+cnn = newspaper.build("https://lite.cnn.io", memoize_articles=False)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    article = cnn.articles[0]
+    article.download()
+    article.parse()
+    return article.text
 
 if __name__ == "__main__":
     app.run()
