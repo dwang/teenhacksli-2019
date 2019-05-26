aba64bad5990c37c8fb2c29471a615ddc689fd0a
Daniel Wang <hello@danielwang.dev>
2019-05-26 07:22:28 -0400

    Revert "categorization displayed"
    This reverts commit 163bda04eee3f224ac463c230019b83735b4abfd.

--- server/app.py
+++ server/app.py
@@ -8,12 +8,12 @@ allowed_categories = ['Jobs & Education', 'Law & Government', 'News', 'Business 
 count = 0
 today = datetime.datetime.now()
 index = open("index.html", "w")
-header = '<!doctype html><meta charset=utf-8><meta cpontent="width=device-width,initial-scale=1,viewport-fit=cover" name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
+header = '<!doctype html><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover" name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
 index.write(header)
 index.write("<h1>Weather</h1>")
-index.write("<p>" + basicWeather.getWeather() + "</>")
+index.write("<p>" + basicWeather.getWeather() + "</p>")
 index.write("<h1>Latest News</h1>")
-indexes = []
+
 for article in cnn.articles:
     try:
         article.download()
@@ -25,26 +25,17 @@ for article in cnn.articles:
         category = list(content_classification.classify(article_text).keys())[0].split("/")[1]
         if category not in allowed_categories:
             continue
-    
-        subheader = category
 
         file_name = pattern.sub('', article_title.replace(" ", "-")).lower() + ".html"
 
         with open("articles/" + file_name, 'w') as out:
-            out.write(header + "<h1>" + article_title + "</h1> <br> <h3>" + subheader + "</h3>" +  "<p class='text'>" + article_text + "</p>")
+            out.write(header + "<h1>" + article_title + "</h1> <br> <p class='text'>" + article_text + "</p>")
 
-        indexes.append(subheader + " - " + article_title)
-        
+        index.write("<a href='articles/{}'>".format(file_name) + article_title + "</a><br><br>")
+
         count += 1
-    except Exception:
-        pass
-
-indexes.sort()
-index.write("<h2>" + indexes[0][0: indexes[0].index("-") - 1] +"</h2>")
-for i in range (0, len(indexes) - 2):
-    index.write("<a href='articles/{}'>".format(pattern.sub('', indexes[i][indexes[i].index("-") + 2:].replace(" ", "-")).lower() + ".html") + indexes[i] + "</a><br><br>" )
-    if(indexes[i][0] != indexes[i+1][0]):
-        index.write("<h2>" + indexes[i+1][0:(indexes[i+1].index("-")-1)] + "</h2>")
+    except Exception as e:
+        print(e)
 
 index.write("<h3>Last Updated: " + today.strftime("%b %d %Y %I:%M:%S") + "</h3>")
 index.write("</main></body></html>")
