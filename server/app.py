69fab47e770e2ca48bfaf22d5dbbb89fa383400c
Daniel Wang <hello@danielwang.dev>
2019-05-26 10:23:09 -0400

    refactor: organize code

--- server/app.py
+++ server/app.py
@@ -1,27 +1,32 @@
-import newspaper, basicWeather, content_classification, datetime
+import newspaper
+import basic_weather
+import content_classification
+import datetime
 import re
 import string
 
 cnn = newspaper.build("https://lite.cnn.io", memoize_articles=False)
 pattern = re.compile(r"[^a-zA-Z0-9-]")
-allowed_categories = ['Jobs & Education', 'Law & Government', 'News', 'Business & Industrial', 'People & Society', 'Finance', 'Health']
-count = 0
-today = datetime.datetime.now()
-header = '<!doctype html><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover" name=viewport><title>News</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style><main>'
+
+header = '<!doctype html><meta charset=utf-8><meta content="width=device-width,initial-scale=1,viewport-fit=cover" name=viewport><title>MirrorDashboard</title><style>body{font-family:Arial,Helvetica,sans-serif;background-color:#fafafa}main{max-width:70ch;padding:2ch;margin:auto}.text{line-height:1.4}a{text-decoration:none;outline:0}</style></head><main><h1><a href="index.html">MirrorDashboard</a></h1><br>'
+
 index = open("index.html", "w")
-index.write(header + '<h1> <a href="weather.html">Weather</a></h1>')
+index.write(header + '<h1><a href="weather.html">Weather</a></h1>')
+
 weather = open("weather.html", "w")
-weather.write(header + "<h1> Weather <br> <br>"  + "<h2>" + basicWeather.getWeather() + "</h2>" + "</h1>")
+weather.write(header + "<h1>Weather</h1><p>" + basic_weather.get_weather() + "</p></main></body></html>")
+
 news = open('news.html','w')
-news.write(header + "<h1> Latest News </h1>")
-index.write('<h1> <a href="news.html">Latest News</a></h1> <h1> <a href="firstaid.html">First Aid</a></h1>')
+news.write(header + "<h1>Latest News</h1>")
+
+index.write('<h1><a href="news.html">Latest News</a></h1> <h1> <a href="firstaid.html">First Aid</a></h1>')
+
 with open('firstaid.html', 'w') as f:
     with open('firstaid.txt', 'r') as txt:
-        content = txt.read()
         f.seek(0, 0)
-        
-        f.write(header.rstrip('\r\n') + '\n' + content)
+        f.write(header.rstrip('\r\n') + '\n' + txt.read())
 
+count = 0
 
 for article in cnn.articles:
     try:
@@ -31,22 +36,25 @@ for article in cnn.articles:
         article_title = article.html.split('<h2 style="margin-top:0px;">')[1].split('</h2>')[0]
         article_text = article.html.split('</div><div><p>')[1].split('</p></div></div>')[0]
 
+        print("[{} / {}] {}".format(count, len(cnn.articles), article_title))
+
+        count += 1
+
         category = list(content_classification.classify(article_text).keys())[0].split("/")[1]
-        if category not in allowed_categories:
+        if category not in ['Jobs & Education', 'Law & Government', 'News', 'Business & Industrial', 'People & Society', 'Finance', 'Health']:
             continue
 
         file_name = pattern.sub('', article_title.replace(" ", "-")).lower() + ".html"
 
         with open("articles/" + file_name, 'w') as out:
-            out.write(header + "<h1>" + article_title + "</h1> <br> <p class='text'>" + article_text + "</p>")
+            out.write(header + "<h1>" + article_title + "</h1><br><p class='text'>" + article_text + "</p>")
 
         news.write("<a href='articles/{}'>".format(file_name) + article_title + "</a><br><br>")
-
-        count += 1
     except Exception as e:
-        print(e)
+        pass
 
-index.write("<h3>Last Updated: " + today.strftime("%b %d %Y %I:%M:%S") + "</h3>")
+index.write("<h3>Last Updated: " + datetime.datetime.now().strftime("%b %d %Y %I:%M:%S") + "</h3></main></body></html>")
 index.close()
+
 news.write("</main></body></html>")
 news.close()
--- server/basic_weather.py
+++ server/basic_weather.py
@@ -3,16 +3,9 @@ import json
 api_key = "708ca3f25f4dfda3c791b09ef4f990aa"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# Give city name
 city_name = "Long Island"
 
-# complete_url variable to store
-# complete url address
-
-
-def getWeather():
-
-
+def get_weather():
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
     response = requests.get(complete_url)
     x = response.json()
@@ -24,7 +17,7 @@ def getWeather():
         current_humidiy = y["humidity"]
         z = x["weather"]
         weather_description = z[0]["description"]
-        return "City = " + city_name + ", NY" + "</br>"+  " Temperature = " + str(round(current_temperature + -273.15, 2)) + " Celsius</br> Atmospheric Pressure = " + str(current_pressure) + " hPa</br> Humidity = " + str(current_humidiy) + "%</br> Description = " + str(weather_description)
+        return "<b>City:</b> " + city_name + ", NY" + "</br>"+ "<b>Temperature:</b> " + str(round(current_temperature + -273.15, 2)) + " Celsius</br><b>Atmospheric Pressure:</b> " + str(current_pressure) + " hPa</br><b>Humidity:</b> " + str(current_humidiy) + "%</br><b>Description:</b> " + str(weather_description)
 
     else:
-        return " City Not Found "
+        return "City not found"
