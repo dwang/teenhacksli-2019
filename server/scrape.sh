9a3d2d7ffdd0c4badea33fe34858946f15e1ed72
Daniel Wang <hello@danielwang.dev>
2019-05-26 10:24:51 -0400

    fix: include other html files

--- server/scrape.sh
+++ server/scrape.sh
@@ -4,3 +4,7 @@ cd /home/dwang/teenhacksli-2019/server
 python3 /home/dwang/teenhacksli-2019/server/app.py
 rsync -a /home/dwang/teenhacksli-2019/server/articles /var/www/html
 rsync /home/dwang/teenhacksli-2019/server/index.html /var/www/html
+rsync /home/dwang/teenhacksli-2019/server/weather.html /var/www/html
+rsync /home/dwang/teenhacksli-2019/server/news.html /var/www/html
+rsync /home/dwang/teenhacksli-2019/server/firstaid.html /var/www/html
+
